b75f34d2d4ce48b15049a6b1f05ae841855b1f29
Ian Andersen <ianderse@mac.com>
2014-12-16 13:42:27 -0700

    add event model; belongs to habit; habit has many events; start of algorithm for streak

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,5 +1,21 @@
 class Habit < ActiveRecord::Base
   validates :name, presence: true
-  #belongs_to :user
-  #has_many :events
+  belongs_to :user
+  has_many :events
+
+  def streak_days
+    # self.events.where(completed: true) # && days in a row
+    # I have no idea if this works.
+    counter = 0
+    day = Date.yesterday
+    event = self.events.last
+    if !event.nil?
+      while event.completed != false do
+        event = self.events.where(created_at: (day-1.day)).first
+        day = day - 1.day
+        counter += 1
+      end
+    end
+    counter
+  end
 end
