2f060890fa94cfdb21c962fb19b4708c7a40c6ec
awortham <awortham@newlifechurch.tv>
2014-12-16 11:39:53 -0700

    we r omniauthed

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ActiveRecord::Base
+  validates :provider,
+  :uid,
+  :name,
+  :image,
+  :oauth_token,
+  :oauth_secret, presence: true
+
+  def self.from_omniauth(auth)
+    where(uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.image = auth.info.image
+      user.oauth_token = auth.credentials.token
+      user.oauth_secret = auth.credentials.secret
+      user.save!
+    end
+  end
+end
