de8d6d11b2e0bb3098d77fe1e32f1a10fee73b11
Ian Andersen <ianderse@mac.com>
2014-12-17 15:21:25 -0700

    displays github repos for user

--- app/assets/javascripts/habit_scripts.js
+++ app/assets/javascripts/habit_scripts.js
@@ -1,5 +1,10 @@
 
- $('#create-custom').click(function() {
-    $('#create-habit-form').toggle() 
+$('#create-custom').click(function() {
+    $('#create-habit-form').toggle()
 });
+
+$('#github-stats-toggle').click(function() {
+    $('#github-stats').toggle()
+});
+
 
