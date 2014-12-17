c09a46d3c105b96db2f7e2beef3525a43f432189
Ian Andersen <ianderse@mac.com>
2014-12-17 13:54:43 -0700

    WIP

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -8,13 +8,14 @@ class Habit < ActiveRecord::Base
     # I have no idea if this works.  Thinking we might need to get more specific with
     # the date (we need to see if there was a completed event at any point during the day)
     counter = 0
-    day = Date.yesterday
-    event = self.events.last
-    if !event.nil?
+    day = Date.yesterday.strftime("%d %b. %Y")
+    events = self.events
+    if !events.nil?
       while event.completed != false do
         event = self.events.where(created_at: (day-1.day)).first
         day = day - 1.day
         counter += 1
+        puts counter
       end
     end
     counter
--- spec/models/habit_spec.rb
+++ spec/models/habit_spec.rb
@@ -1,5 +1,5 @@
 describe 'habit' do
-  
+
   before do
     @habit = Habit.new
   end
@@ -14,5 +14,22 @@ describe 'habit' do
     @habit.name = ""
     @habit.save
     expect(@habit).to_not be_valid
+  end
+
+  it 'can find its own event streak' do
+    @habit.name = "Meditation"
+    @habit.save
+    5.times do
+      @habit.events.create(completed: true)
+    end
+
+    @habit.events.last.created_at = "2014-12-17 20:40:46.855146"
+    @habit.events.last.save
+    @habit.events.fourth.created_at = "2014-12-16 20:40:46.855146"
+    @habit.events.fourth.save
+    @habit.events.third.created_at = "2014-12-15 20:40:46.855146"
+    @habit.events.third.save
+
+    expect(@habit.streak_days).to eq(3)
   end
 end
