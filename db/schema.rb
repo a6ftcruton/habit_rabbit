98fc4fd3d47dfdcab5d9f4d643de444f86043ea2
awortham <awortham@newlifechurch.tv>
2014-12-18 09:54:10 -0700

    added password to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141217215632) do
+ActiveRecord::Schema.define(version: 20141218165316) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141217215632) do
     t.string   "oauth_secret"
     t.string   "email_address"
     t.string   "github_name"
+    t.string   "password_digest"
   end
 
 end
