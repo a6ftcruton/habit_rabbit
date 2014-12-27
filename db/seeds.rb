8fc32b2ce6961e111b6e291f8a066a1ff727ca0f
Jim Sutton <jimsuttonjimsutton@gmail.com>
2014-12-27 14:42:51 -0700

    Test user habit association including login

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,12 @@ User.create!(
     oauth_token: "fjdkslfaPOI", 
     oauth_secret: "abc123"
 )
+puts "Planted User seeds"
 
+#  HABIT SEEDS 
+#-----------------------
+Habit.create!(name: "first exercise", user_id: "1") 
+Habit.create!(name: "second exercise", user_id: "1") 
+Habit.create!(name: "I will read things", user_id: "1") 
+
+puts "Planted Habit seeds"
