189b87b6cf6e876c3ab2c3c162185ffbc010e27a
awortham <wortham.aaron@gmail.com>
2015-01-06 15:58:54 -0700

    refactored event confirmation spec-still one change to go

--- spec/features/event_confirmation_spec.rb
+++ spec/features/event_confirmation_spec.rb
@@ -4,26 +4,26 @@ describe 'user event confirmation', type: :feature do
   include Capybara::DSL
 
   before do
-    @user = User.create(name: "Joe", email_address: "stuff@whatup.com", password: 'password', password_confirmation: 'password')
+    @user = create(:user)
     page.set_rack_session(user_id: @user.id)
-    @habit = Habit.create!(user_id: @user.id, name: "pushups", start_date: Time.now)
+    @habit = Habit.create(user_id: @user.id, name: "pushups", start_date: DateTime.now)
   end
 
   it 'has confirmation option for each habit' do
-    visit '/dashboard' 
+    visit '/dashboard'
     expect(page).to have_content @habit.name
     click_on 'YES'
-    expect(current_path).to eq dashboard_path 
-    expect(page).to have_content "Thanks! We've updated your streak."      
+    expect(current_path).to eq dashboard_path
+    expect(page).to have_content "Thanks! We've updated your streak."
   end
 
   context 'clicking yes or no' do
 
     it 'creates a new habit' do
       events = Habit.last.events.count
-      visit '/dashboard' 
+      visit '/dashboard'
       click_on 'NO'
-      expect(current_path).to eq dashboard_path 
+      expect(current_path).to eq dashboard_path
       updated_events = Habit.last.events.count
       expect(updated_events).to eq (events + 1)
     end
@@ -32,12 +32,10 @@ describe 'user event confirmation', type: :feature do
       visit '/dashboard'
       expect(page).to have_css('.event-form')
       click_on 'YES'
-      expect(current_path).to eq dashboard_path 
+      expect(current_path).to eq dashboard_path
       save_and_open_page
       expect(page).to_not have_css('.event-form')
     end
   end
 
 end
-
-
