896c3abf13f7bce5f7fcdb31450e4887f342f0d6
Ian Andersen <ianderse@mac.com>
2015-01-07 09:10:07 -0700

    remove the reverse from current_streak_days

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -11,7 +11,7 @@ class Habit < ActiveRecord::Base
   end
 
   def current_streak_days
-    unless streaks.empty? || !sorted_events_for_habit.reverse.last.completed
+    unless streaks.empty? || !sorted_events_for_habit.last.completed
       streaks.first.days
     else
       0
